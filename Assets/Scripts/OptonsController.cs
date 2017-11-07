f52460e03b3a7376d3ce328361dd7d31b43ef80f
Mike Talley <miketalley@hotmail.com>
2017-11-07 11:10:51 -0500

    commiting before changing music manager mute functionality

---
+++ Assets/Scripts/OptonsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptonsController : MonoBehaviour {
+    public LevelManager levelManager;
+    public Slider volumeSlider;
+    public Slider difficultySlider;
+
+    private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+        musicManager = GameObject.FindObjectOfType<MusicManager>();
+
+        volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+        difficultySlider.value = PlayerPrefsManager.GetDifficulty();
+	}
+
+    void Update ()
+    {
+        musicManager.ChangeVolume(volumeSlider.value);
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsManager.SetMasterVolume(volumeSlider.value);
+        PlayerPrefsManager.SetDifficulty(difficultySlider.value);
+
+        levelManager.LoadScene("Start");
+    }
+}
---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFFICULTY_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+    // level_unlocked_1
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume > 0f && volume < 1f)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Master volume out of range");
+        }
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+	public static void UnlockLevel(int level)
+    {
+        if (level > 0 && level < SceneManager.sceneCountInBuildSettings)
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), 1); //  1 for true
+        }
+        else
+        {
+            Debug.LogError("Trying to unlock level not in build order");
+        }
+    }
+
+    public static bool IsLevelUnlocked(int level)
+    {
+        if (level < SceneManager.sceneCountInBuildSettings)
+        {
+            return PlayerPrefs.GetInt(LEVEL_KEY + level.ToString()) == 1;
+        }
+        else
+        {
+            Debug.LogError("Level query was out of range of existing levels");
+            return false;
+        }
+    }
+
+    public static void SetDifficulty(float difficulty)
+    {
+        if (difficulty > 0f && difficulty < 1f)
+        {
+            PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficulty);
+        }
+        else
+        {
+            Debug.LogError("Master volume out of range");
+        }
+    }
+
+    public static float GetDifficulty()
+    {
+        return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+    }
+}
